a78869bac161159192f91f3659a2627a1a107624
Ful4nito <jonathanb.9724@gmail.com>
2025-07-06 06:31:03 -0600

    ETS Calculo

--- Semestre [2]/Algoritmos y Estructuras de Datos/Practicas C/Practica13.cpp
+++ Semestre [2]/Algoritmos y Estructuras de Datos/Practicas C/Practica13.cpp
@@ -18,21 +18,26 @@ struct lista {
 
 nodo *raiz;
 FILE *F;
+
 int id = 0;
+char dato_nombre[25];
+char dato_pregunta[100];
 
 int main () {
   raiz = new nodo;
   raiz = NULL;
-    
+  
+  printf("\n Abriendo archivo .... ");
   F = fopen("conocimiento.txt", r);
   if (F == NULL) {
     printf("\n Error al abrir el archivo.");
     return 1;
   }
+  printf(" Correcto");
 
-  fscanf(archivo, "%d.%25[^.].%100[^\n]", &id, nombre, pregunta)
+  printf("\n Obteniendo datos de Fila 1");
+  fscanf(F, "%d.%25[^.].%100[^\n]", &id, dato_nombre, dato_pregunta);
+  printf("\n %d  %s  %s", id, dato_nombre, dato_pregunta);
 
-  fscanf(,"");
-
-  printf("\n Nodo raiz inicializado a NULL");
+  fclose(F);
 }
